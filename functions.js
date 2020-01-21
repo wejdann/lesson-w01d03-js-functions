60bfc47db49c56e9f775724ac354fe93467a48ec
wejdann <weejdansaud58@hotmail.com>
2020-01-21 17:03:48 +0300

    wojdan Alrasheedi

---
+++ functions.js
@@ -0,0 +1,19 @@
+//1 done 
+const youRock=function(name1){
+    return name1 ;
+      console.log("You rock" +"  " + name1);}
+
+      //2done
+ const square =function(num) {
+        return result = num *num;
+        
+        }
+        //3 done
+const cube =function(no) {
+     return result = no * no *no;
+              }
+//4 done
+const toTheFourth =function(num4) {
+    return result = num4*num4*num4*num4;
+    
+    }
